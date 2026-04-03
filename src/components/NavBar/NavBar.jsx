c1e55ffedbd45fe1b8aa67811a2be0e74c53780a
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-04-04 00:00:42 +0600

    assignment 06 navbar done, lucide installed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,50 +1,53 @@
+import { ShoppingCart } from 'lucide-react';
 import React from 'react';
+
+const navLinks = ["Products", "Features", "Pricing", "Testimonials", "FAQ"];
+
 
 const NavBar = () => {
     return (
-        <nav>
-            <div className="navbar bg-base-100 shadow-sm">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <nav className='py-6 border-b border-[#F2F2F2]'>
+            <div className="flex items-center justify-between bg-base-100 max-w-300 w-11/12 lg:w-3/4 mx-auto p-0">
+                <div className="flex items-center gap-4">
+                    <div className="dropdown xl:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost px-0">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
                         <ul
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+
+                            {
+                                navLinks.map((link, index) => <li key={index}><a className='text-base font-semibold leading-[120%] text-black10'>{link}</a></li>)
+                            }
+
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+
+                    <div className='h-fit'>
+                        <a className="text-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-bold">DigiTools</a>
+                    </div>
                 </div>
-                <div className="navbar-center hidden lg:flex">
+
+                <div className="hidden xl:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2 bg-base-100 w-40 z-1">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                            navLinks.map((link, index) => <li key={index}><a className='text-base font-semibold leading-[120%] text-black10'>{link}</a></li>)
+                        }
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+
+                <div className="">
+                    <div className='flex items-center gap-4'>
+                        <ShoppingCart size={16} />
+
+                        <button className='text-base font-semibold leading-[120%] text-black10'>Login</button>
+
+                        <button className="btn rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-semibold h-fit py-3.25 px-4">Get Started</button>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </nav >
     );
 };
 
